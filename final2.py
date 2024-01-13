8942c477e30d0cbc396c5b4a7ed8dc551eb3e853
saa51 <1700012800@pku.edu.cn>
2024-01-14 00:05:47 +0800

    add hazard function; add simulation plot

--- final2.py
+++ final2.py
@@ -1,7 +1,55 @@
 from menu_cost import MenuCostEnv
+import numpy as np
+import matplotlib.pyplot as plt
 
+epsilon = 1e-6
+def get_panel_data(p):
+    n, T = p.shape
+    p = np.concatenate((np.zeros((n, 1)), p), axis=1)
+    freq_inc = p[:, 1:] - p[:, :-1] > epsilon
+    freq_inc = np.average(freq_inc.reshape((n, -1, 12)), axis=-1)
+    freq_dec = p[:, 1:] - p[:, :-1] < -epsilon
+    freq_dec = np.average(freq_dec.reshape((n, -1, 12)), axis=-1)
+    freq_change = np.abs(p[:, 1:] - p[:, :-1]) > epsilon
+    freq_change = np.average(freq_change.reshape((n, -1, 12)), axis=-1)
+
+    size_inc_ = np.maximum(p[:, 1:] - p[:, :-1], 0)
+    size_inc = np.zeros(freq_inc.shape)
+    size_inc[freq_inc > 0] = np.average(size_inc_.reshape((n, -1, 12)), axis=-1)[freq_inc > 0] / freq_inc[freq_inc > 0]
+    size_dec_ = np.minimum(p[:, 1:] - p[:, :-1], 0)
+    size_dec = np.zeros(freq_inc.shape)
+    size_dec[freq_dec > 0] = np.average(size_dec_.reshape((n, -1, 12)), axis=-1)[freq_dec > 0] / freq_dec[freq_dec > 0]
+    size_change_ = np.abs(p[:, 1:] - p[:, :-1])
+    size_change = np.zeros(freq_inc.shape)
+    size_change[freq_change > 0] = np.average(size_change_.reshape((n, -1, 12)), axis=-1)[freq_change > 0] / freq_change[freq_change > 0]
+
+    return freq_inc, freq_dec, size_inc, size_dec, freq_change, size_change
 
 if __name__ == '__main__':
     env = MenuCostEnv()
-    env.vfi()
+    res = env.vfi()
     env.plot_policy()
+    p, a, cpi, eta, p_paths, _, eta_paths = env.simulate()
+    data = list(get_panel_data(p))
+    cpi = np.concatenate((np.zeros((cpi.shape[0], 1)), cpi), axis=1)
+    cpi = np.sum((cpi[:, 1:] - cpi[:, :-1]).reshape((cpi.shape[0], -1, 12)), axis=-1)
+    data.append(cpi)
+    labels = ['Frequency of price increase', 'Frequency of price decrease', 
+            'Size of price increase', 'Size of price decrease', 'Frequency of price change',
+            'Size of price change', 'CPI', ]
+    for item, label in zip(data, labels):
+        seq = np.average(item, axis=0)
+        if 'change' in label:
+            continue
+        plt.plot(seq, label=label)
+    plt.xlabel('Time')
+    plt.legend()
+    plt.show()
+
+    # Feel free to use Stata to run regression
+
+    initial_prob = np.zeros(res.value.shape)
+    initial_prob[int(res.value.shape[0] / 2), int(res.value.shape[1] / 2)] = 1
+    hazard = env.hazard_function(initial_prob=initial_prob, length=100)
+    plt.plot(hazard)
+    plt.show()
--- menu_cost.py
+++ menu_cost.py
@@ -11,12 +11,15 @@ import matplotlib.pyplot as plt
 class MenuCostVFIRes:
     a_grid: np.ndarray
     trans_mat_a: np.ndarray
+    eta_grid: np.ndarray
+    eta_prob: np.ndarray
     p_grid: np.ndarray
     trans_mat_p: np.ndarray
     policy_grid: np.ndarray
     value: np.ndarray
     policy: np.ndarray
     offset: int
+    precision: int
 
 
 class MenuCostEnv:
@@ -72,21 +75,22 @@ class MenuCostEnv:
     state: p_t = log (p_{t-1}(z) / P_t), a_t = log(A_t)
     policy: the center of p_{t+1} = log (p_t(z) / P_t) - mu
     '''
-    def vfi(self, a_num=5, p_num=201, eta_num=5, eta_precision=1):
+    def vfi(self, a_num=5, p_num=201, eta_num=5, eta_precision=1, gamma=0):
         assert (eta_num - 1) % (2 * eta_precision) == 0
         assert (p_num - 1) % (2 * eta_precision) == 0
         a_grid, trans_mat_a = Tauchen(self.rho, self.sig_ep ** 2, 0).approx(n=a_num)
         p_width = (p_num - 1) / 2 / eta_precision * self.mu
         p_grid = np.linspace(-p_width, p_width, p_num)
-        _, trans_prob_eta = self.approx_eta(eta_num, eta_precision)
+        eta_grid, trans_prob_eta = self.approx_eta(eta_num, eta_precision)
         policy_num = p_num - eta_num + 1
         offset = int(eta_num / 2)
         policy_grid = p_grid[offset: -offset]
 
         profit = np.exp(-self.theta * (policy_grid + self.mu)) * (np.exp(policy_grid + self.mu)
                  - np.expand_dims((self.theta - 1) / self.theta * np.exp(-a_grid), axis=-1).repeat(policy_num, axis=-1))
-        profit -= (self.theta - 1) / self.theta * self.k
+        profit = profit - (self.theta - 1) / self.theta * self.k
         pi = np.tile(profit, (p_num, 1, 1))
+        pi -= gamma / 2 * np.exp(2 * (policy_grid + self.mu))
         for i in range(policy_num):
             if offset + i + eta_precision >= p_num:
                 break
@@ -102,11 +106,11 @@ class MenuCostEnv:
         while not np.allclose(value, old_v):
             old_v = np.copy(value)
             value = np.max(pi + self.beta * trans_mat_a @ value.T @ trans_mat_p.T, axis=-1)
-            print(f'iter {iter}, loss {np.max(value - old_v)}')
+            print(f'iter {iter}, loss {np.abs(np.max(value - old_v))}')
             iter += 1
 
         policy = np.argmax(pi + trans_mat_a @ value.T @ trans_mat_p.T, axis=-1)
-        res = MenuCostVFIRes(a_grid, trans_mat_a, p_grid, trans_mat_p, policy_grid, value, policy, offset)
+        res = MenuCostVFIRes(a_grid, trans_mat_a, eta_grid, trans_prob_eta, p_grid, trans_mat_p, policy_grid, value, policy, offset, eta_precision)
         self.res = res
         return res
 
@@ -119,7 +123,7 @@ class MenuCostEnv:
         plt.plot(res.p_grid, res.p_grid)
         plt.title('Policy' if title is None else title)
         plt.xlabel('log[ p_{t-1}(z)/P_t ]')
-        plt.xlabel('log[ p_t(z)/P_t ]')
+        plt.ylabel('log[ p_t(z)/P_t ]')
         plt.legend()
         if figure_path:
             plt.savefig(figure_path)
@@ -128,18 +132,59 @@ class MenuCostEnv:
         else:
             plt.clf()
     
-    def simulate(n=10000, T=120, res=None):
+    def simulate(self, n=10000, T=120, res=None, annualize=False):
         if res is None:
             res = self.res
         assert res is not None
         a_path, _ = FiniteMarkov(res.a_grid, res.trans_mat_a).simulate(T, 0)
-        p_paths = np.ones((n, T), dtype=int) * int(res.value.shape[0] / 2)
-        a_paths = np.ones((n, T), dtype=int) * int(res.value.shape[1] / 2)
+        p_paths = np.ones((n, T + 1), dtype=int) * int(res.value.shape[0] / 2)
+        a_paths = np.ones((n, T + 1), dtype=int) * int(res.value.shape[1] / 2)
+        eta_paths = np.zeros((n, T + 1), dtype=int)
+        cpi_paths = np.zeros((n, T + 1))
+        # using the sample aggregate price path P_t to keep average P_t fluctuation
+        eta_paths[:, 1:] = np.random.choice(len(res.eta_grid), size=(T), p=res.eta_prob)
+        cpi_paths[:, 1:] = np.cumsum(res.eta_grid[eta_paths[:, 1:]] + self.mu, axis=1)
+        offset = int(len(res.eta_grid) / 2)
         for i in tqdm(range(n)):
-            a_path, _ = FiniteMarkov(res.a_grid, res.trans_mat_a).simulate(T - 1, 0)
-            a_paths[i] =  a_path
-            for t in range(1, T):
-                p_paths[i, t] = np.random.choice(list(range(p_num)), p=res.trans_mat_p[res.policy[p_paths[i, t - 1], a_path[t - 1]]])
-        return res.p_grid[p_paths], res.a_grid[a_paths], p_paths, a_paths
+            a_path, _ = FiniteMarkov(res.a_grid, res.trans_mat_a).simulate(T, 0)
+            a_paths[i] = a_path
+        for t in range(1, T + 1):
+            # 1 offset for policy grid -> p grid, 1 offset for centralizing eta grid
+            p_paths[:, t] = 2 * offset + res.policy[(p_paths[:, t - 1], a_paths[:, t - 1])] - eta_paths[:, t]
+        if annualize:
+            return self.annualize(res.p_grid[p_paths][:, 1:] + cpi_paths[:, 1:], res.a_grid[a_paths][:, 1:], cpi_paths[:, 1:], res.eta_grid[eta_paths][:, 1:]) + \
+                    (p_paths[:, 1::12], a_paths[:, 1::12], eta_paths[:, 1:])
+        return res.p_grid[p_paths][:, 1:] + cpi_paths[:, 1:], res.a_grid[a_paths][:, 1:], cpi_paths[:, 1:], res.eta_grid[eta_paths][:, 1:], p_paths[:, 1:], a_paths[:, 1:], eta_paths[:, 1:]
     
-    
+    def annualize(self, *args):
+        def annual_map(data):
+            n, T = data.shape
+            data = data.reshape((n, int(T / 12), 12))
+            return data.sum(axis=-1)
+        return tuple(map(annual_map, list(args)))
+
+    def hazard_function(self, length=25, res=None, initial_prob=None):
+        if res is None:
+            res = self.res
+        assert res is not None
+
+        if initial_prob is None:
+            prob = np.ones((res.value.shape))
+            prob = prob / np.sum(prob)
+        else:
+            prob = initial_prob
+
+        hazard = np.zeros(length)
+        stay_put = np.equal((res.policy + res.precision + res.offset).T, np.arange(len(res.p_grid))).T
+
+        trans_p_stay = np.zeros((len(res.p_grid), len(res.p_grid)))
+        for i in range(len(res.p_grid)):
+            if 0 <= i - res.offset and i + res.offset < len(res.p_grid):
+                trans_p_stay[i, i - res.offset:i - res.offset + len(res.eta_grid)] = res.eta_prob
+
+        for i in range(length):
+            survival = stay_put * prob
+            hazard[i] = 1 - np.sum(survival) / np.sum(prob)
+            prob = trans_p_stay.T @ survival @ res.trans_mat_a
+        
+        return hazard
